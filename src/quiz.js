a50fa99863051eac79111307f1e4b5aa9b3be6cd
dillandatanerd <dillanpatelnz@gmail.com>
2025-07-28 16:58:31 +0200

    upto has ended method for quiz class

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,60 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
 
-    // 4. shuffleQuestions()
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+    this.currentQuestionIndex = 0;
+  }
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+  // Checks if the passed answer is correct for the current question and increments correctAnswers by 1 if the answer is correct.
+
+  // should be defined.
+  // should be a function.
+  // should receive 1 argument (answer - string).
+  // should increase correctAnswers by 1 when called with a correct answer for the current question
+
+  checkAnswer(answer) {
+    if (this.questions[this.currentQuestionIndex].answer === answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex < (this.questions.length)) {
+      return false;
+    } else if (this.currentQuestionIndex === (this.questions.length )){
+      return true;
+    }
+  }
 }
+
+// should return 'false' when 'currentQuestionIndex' is less than the 'questions' array length
+// should return 'true' when 'currentQuestionIndex' is equal to the 'questions' array length
