f493fc4ec408d918bfacfe68ffb7471b12a090a5
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-07-28 16:58:20 +0200

    Question class implemented

--- src/question.js
+++ src/question.js
@@ -4,4 +4,22 @@ class Question {
     // 1. constructor (text, choices, answer, difficulty)
 
     // 2. shuffleChoices()
+    constructor (text, choices, answer, difficulty){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+    shuffleChoices(){
+        let arr = this.choices // or this.choices.splice()
+        for (let i = arr.length - 1; i > 0; i--) {
+            // Pick a random index from 0 to i
+            const j = Math.floor(Math.random() * (i + 1));
+
+            // Swap arr[i] with arr[j]
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    }
 }
