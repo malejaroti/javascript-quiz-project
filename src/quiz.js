c4a8085125795ed75dd62e5ccb3cd4556110db41
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-07-29 13:25:47 +0200

    Day 2 done

--- src/quiz.js
+++ src/quiz.js
@@ -54,7 +54,47 @@ class Quiz {
       return true;
     }
   }
+  filterQuestionsByDifficulty(difficulty){
+    if(difficulty > 1  && difficulty < 3){
+      this.questions = this.questions.filter( question => question.difficulty === difficulty)
+    }
+  }
+  // averageDifficulty(){
+  //   return this.questions.reduce( (sum, ele) =>  (sum + ele.difficulty))
+  // }
+  averageDifficulty(){ 
+    let sum = this.questions.reduce( (sum, ele) =>  (sum + ele.difficulty),0)
+    return sum / this.questions.length
+  }
+
 }
+
+class Question {
+    // YOUR CODE HERE:
+    //
+    // 1. constructor (text, choices, answer, difficulty)
+
+    // 2. shuffleChoices()
+    constructor (text, choices, answer, difficulty){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+
+    shuffleChoices(){
+        let arr = this.choices // or this.choices.splice()
+        for (let i = arr.length - 1; i > 0; i--) {
+            // Pick a random index from 0 to i
+            const j = Math.floor(Math.random() * (i + 1));
+
+            // Swap arr[i] with arr[j]
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    }
+}
+
 
 // should return 'false' when 'currentQuestionIndex' is less than the 'questions' array length
 // should return 'true' when 'currentQuestionIndex' is equal to the 'questions' array length
